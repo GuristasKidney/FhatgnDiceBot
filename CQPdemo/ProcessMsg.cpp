c5dc27f4182fe6e1e03ae458704dea355601626b
GuristasKidney <236820311@qq.com>
2017-12-14 15:11:51 +0800

    正式版本2.00
    重写.r命令逻辑
    添加暗骰功能
    添加.help命令

--- CQPdemo/ProcessMsg.cpp
+++ CQPdemo/ProcessMsg.cpp
@@ -5,7 +5,9 @@
 #include "mtrand.hpp"
 #include <string.h>
 #include <timeapi.h>
+#include <shlwapi.h>
 
+#pragma comment(lib,"shlwapi.lib")
 #pragma comment(lib,"winmm.lib")
 
 static Process g_Process;
@@ -58,39 +60,37 @@ int Process::ProcessMsg(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromD
 		strcpy(buf,tmp);
 	}
 
+	//.r命令
+	if (0 == strnicmp(buf, r, strlen(r)))
+	{
+		return RollSegmentation(ac, fromQQ, fromGroup, fromDiscuss, buf);
+	}
+
+	//.help命令
+	if (0 == strnicmp(buf, help, strlen(help)))
+	{
+		char helpmsg[] = "<FhatgnDice　version:2.0.0 by 腰子>\n\
+< 指令支持 >\n\
+- 骰点							.r 1d100+3\n\
+- 暗骰私信结果						.rh 1d20\n\
+- 投掷 coc属性						!coc 5\n\
+- 投掷 coc 7版属性						!coc7 5\n\
+- 投掷 本日运势						.jrrp\n\
+- 设置昵称							.nn 新昵称";
+		SendMsg(ac, fromQQ, fromGroup, fromDiscuss, helpmsg);
+		return EVENT_BLOCK;
+	}
+
+	/************************************************************************
+	* old 1.0.0
+	* 待修改：!coc命令合并
+	*         指令判断
+	************************************************************************/
+
 	char *ptr = strtok(buf, " ");
 
 	if (NULL == ptr)
 	{
-		return EVENT_IGNORE;
-	}
-
-	//.rd命令
-	if(0 == stricmp(ptr, rd))
-	{
-		ptr = strtok((char *)NULL, "");
-		return RollDice(ac, fromQQ, fromGroup, fromDiscuss, 1, 100, ptr);
-	}	
-	
-	//.r命令
-	if (0 == stricmp(ptr, r))
-	{
-		ptr = strtok((char *)NULL, "d");
-		unsigned int dicenum = atoi(ptr);
-		int sides = 100;
-		if (1<= dicenum && dicenum <=100)
-		{
-			char* plus = ptr;
-			ptr = strtok((char *)NULL, "d");
-			if (NULL != ptr)
-			{
-				sides = atoi(ptr);
-				ptr = strtok((char *)NULL, " ");
-			}
-
-			return RollDice(ac, fromQQ, fromGroup, fromDiscuss, dicenum, sides, ptr);
-		}
-
 		return EVENT_IGNORE;
 	}
 
@@ -139,76 +139,596 @@ int Process::ProcessMsg(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromD
 	return EVENT_IGNORE;
 }
 
-// 投掷骰子
-int Process::RollDice(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, unsigned int dicenum, unsigned int sides, char* msg)
+// 普通骰点消息处理
+int Process::RollSegmentation(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, char* msg)
 {
 	XAutoLock l(csRollDice);
-	unsigned int sum = 0;
-	unsigned int throwtimes = dicenum;
-	mtsrand((unsigned)(::timeGetTime() + fromQQ));
-
-	char str[2048];
+	char texttmp[2048];
+	char text[2048];
 	int plus = 0;
+	bool secretly = false;
+	char *tmp;
+
+	strcpy(texttmp, msg);
+	strlwr(msg);
+
+	tmp = strtok(msg, ".r");
+
+	if (NULL == tmp)
+	{
+		SendMsg(ac, fromQQ, fromGroup, fromGroup, "您的输入格式有误,请参考帮助指令 .help");
+		return EVENT_BLOCK;
+	}
+
+	if (NULL == strstr(tmp, "d"))
+	{
+		SendMsg(ac, fromQQ, fromGroup, fromGroup, "您的输入格式有误,请参考帮助指令 .help");
+		return EVENT_BLOCK;
+	}
+
+	VS_StrLTrim(tmp);//清除字符串前的空格
+
+	if ('h' == tmp[0])
+	{
+		secretly = true;
+		tmp++;
+	}
+
+	VS_StrLTrim(tmp);//清除字符串前的空格
+
+	while (1)
+	{
+		//没有后续字符串需要处理时直接输出
+		if (NULL == tmp)
+		{
+			return RollDice(ac, fromQQ, fromGroup, fromDiscuss, NULL, plus, secretly);
+		}
+
+		//判断是否含roll点指令
+		if (NULL != strstr(tmp, "d"))
+		{
+			char *cmdL1 = NULL;
+			if ('d' == tmp[0])//strtok不识别字符串首部的分隔符
+			{
+				cmdL1 = tmp + 1;
+			}
+			else
+			{
+				cmdL1 = strtok(tmp, "d");//以d为标签做分割
+			}
+
+			//骰子数默认为1
+			if (NULL == cmdL1)
+			{
+				int dicenum = 1;
+				//获取骰子面数
+				char *cmdL2 = strtok((char*)NULL, "d");
+
+				if (NULL == cmdL2)
+				{
+					diceVect.push_back(std::make_pair(1, 100));
+					tmp = NULL;
+					continue;
+				}
+
+				//分离数字和文字
+				int i = 0;
+				while (1)
+				{
+					if (cmdL2[i] >= '0'&&cmdL2[i] <= '9') //如果数组元素是数字
+					{
+						i++;
+						continue;
+					}
+					else
+					{
+						char side[8];
+						unsigned int sides = 0;
+
+						if (i == 0)
+						{
+							sides = 100;
+						}
+						else
+						{
+							strncpy(side, cmdL2, i);
+							strcpy(side + i, "\0");
+							sides = atoi(side);
+						}
+
+						strcpy(text, cmdL2 + i);
+
+						diceVect.push_back(std::make_pair(dicenum, sides));
+
+						strcpy(tmp, text);
+
+						break;
+					}
+				}
+			}
+			else
+			{
+				//判断首字符
+				if (isdigit(cmdL1[0]))//是数字
+				{
+					if (strspn(cmdL1, "0123456789") == strlen(cmdL1))
+					{
+						int dicenum = atoi(cmdL1);
+
+						//获取骰子面数
+						char *cmdL2 = strtok((char*)NULL, "");
+
+						if (NULL == cmdL2)
+						{
+							diceVect.push_back(std::make_pair(dicenum, 100));
+							tmp = NULL;
+							continue;
+						}
+
+						//分离数字和文字
+						int i = 0;
+						while (1)
+						{
+							if (cmdL2[i] >= '0'&&cmdL2[i] <= '9') //如果数组元素是数字
+							{
+								i++;
+								continue;
+							}
+							else
+							{
+								char side[8];
+								unsigned int sides = 0;
+
+								if (i == 0)
+								{
+									sides = 100;
+								}
+								else
+								{
+									strncpy(side, cmdL2, i);
+									strcpy(side + i, "\0");
+									sides = atoi(side);
+								}
+
+								strcpy(text, cmdL2 + i);
+								strcpy(tmp, text);
+
+								diceVect.push_back(std::make_pair(dicenum, sides));
+								break;
+							}
+						}
+					}
+					else if (0 == stricmp(cmdL1, tmp + 1))//字符串为"d...."格式
+					{
+						int dicenum = 1;
+						//分离数字和文字
+						int i = 0;
+						while (1)
+						{
+							if (cmdL1[i] >= '0'&&cmdL1[i] <= '9') //如果数组元素是数字
+							{
+								i++;
+								continue;
+							}
+							else
+							{
+								char side[8];
+								unsigned int sides = 0;
+
+								if (i == 0)
+								{
+									sides = 100;
+								}
+								else
+								{
+									strncpy(side, cmdL1, i);
+									strcpy(side + i, "\0");
+									sides = atoi(side);
+								}
+
+								strcpy(text, cmdL1 + i);
+								strcpy(tmp, text);
+
+								diceVect.push_back(std::make_pair(dicenum, sides));
+								break;
+							}
+						}
+					}
+					else
+					{
+						//指令错误
+						diceVect.clear();
+						SendMsg(ac, fromQQ, fromGroup, fromGroup,"您的输入格式有误,请参考帮助指令 .help");
+						return EVENT_BLOCK;
+					}
+				}
+				else if (0 == strncmp(cmdL1, "+", strlen("+")) || 0 == strncmp(cmdL1, "-", strlen("-")))//加减法
+				{
+					if (strspn(cmdL1 + 1, "0123456789") == (strlen(cmdL1) - 1))
+					{
+						int dicenum = 1;
+						if (strlen(cmdL1) > 1)//排除+d或-d格式
+						{
+							dicenum = atoi(cmdL1);
+						}
+						else if (0 == strncmp(cmdL1, "-", strlen("-")))
+						{
+							dicenum = -1;
+						}
+
+						//获取骰子面数
+						char *cmdL2 = strtok((char*)NULL, "");
+
+						if (NULL == cmdL2)
+						{
+							diceVect.push_back(std::make_pair(dicenum, 100));
+							tmp = NULL;
+							continue;
+						}
+
+						//分离数字和文字
+						int i = 0;
+						while (1)
+						{
+							if (cmdL2[i] >= '0'&&cmdL2[i] <= '9') //如果数组元素是数字
+							{
+								i++;
+								continue;
+							}
+							else
+							{
+								char side[8];
+								unsigned int sides = 0;
+
+								if (i == 0)
+								{
+									sides = 100;
+								}
+								else
+								{
+									strncpy(side, cmdL2, i);
+									strcpy(side + i, "\0");
+									sides = atoi(side);
+								}
+
+								strcpy(text, cmdL2 + i);
+								strcpy(tmp, text);
+
+								diceVect.push_back(std::make_pair(dicenum, sides));
+								break;
+							}
+						}
+					}
+					else if (0 == stricmp(cmdL1, tmp + 1))//字符串为"d...."格式
+					{
+						int dicenum = 1;
+						//分离数字和文字
+						int i = 0;
+						while (1)
+						{
+							if (cmdL1[i] >= '0'&&cmdL1[i] <= '9') //如果数组元素是数字
+							{
+								i++;
+								continue;
+							}
+							else
+							{
+								char side[8];
+								unsigned int sides = 0;
+
+								if (i == 0)
+								{
+									sides = 100;
+								}
+								else
+								{
+									strncpy(side, cmdL1, i);
+									strcpy(side + i, "\0");
+									sides = atoi(side);
+								}
+
+								strcpy(text, cmdL1 + i);
+								strcpy(tmp, text);
+
+								diceVect.push_back(std::make_pair(dicenum, sides));
+								break;
+							}
+						}
+					}
+					else if (isdigit(cmdL1[1]))//判断首字符是数字
+					{
+						//分离数字和文字
+						int i = 1;
+						while (1)
+						{
+							if (cmdL1[i] >= '0'&&cmdL1[i] <= '9') //如果数组元素是数字
+							{
+								i++;
+								continue;
+							}
+							else
+							{
+								char cplus[8];
+								strncpy(cplus, cmdL1, i);
+								strcpy(cplus + i, "\0");
+								plus = atoi(cplus);
+
+								char* msgtmp = StrStrI(texttmp, text + i);
+								return RollDice(ac, fromQQ, fromGroup, fromDiscuss, msgtmp, plus, secretly);
+							}
+						}
+					}
+					else
+					{
+						char* msgtmp = StrStrI(texttmp, text);
+						return RollDice(ac, fromQQ, fromGroup, fromDiscuss, msgtmp, plus, secretly);
+					}
+				}
+				else if (0 == stricmp(cmdL1, tmp + 1))//字符串为"d...."格式
+				{
+					int dicenum = 1;
+					//分离数字和文字
+					int i = 0;
+					while (1)
+					{
+						if (cmdL1[i] >= '0'&&cmdL1[i] <= '9') //如果数组元素是数字
+						{
+							i++;
+							continue;
+						}
+						else
+						{
+							char side[8];
+							unsigned int sides = 0;
+
+							if (i == 0)
+							{
+								sides = 100;
+							}
+							else
+							{
+								strncpy(side, cmdL1, i);
+								strcpy(side + i, "\0");
+								sides = atoi(side);
+							}
+
+							strcpy(text, cmdL1 + i);
+							strcpy(tmp, text);
+
+							diceVect.push_back(std::make_pair(dicenum, sides));
+							break;
+						}
+					}
+				}
+				else
+				{
+					char* msgtmp = StrStrI(texttmp, text);
+					return RollDice(ac, fromQQ, fromGroup, fromDiscuss, msgtmp, plus, secretly);
+				}
+			}
+		}
+		else
+		{
+			//判断首字符
+			if (0 == strncmp(tmp, "+", strlen("+")) || 0 == strncmp(tmp, "-", strlen("-")))//加减法
+			{
+				if (isdigit(tmp[1]))//是数字
+				{
+					//分离数字和文字
+					int i = 1;
+					while (1)
+					{
+						if (tmp[i] >= '0'&&tmp[i] <= '9') //如果数组元素是数字
+						{
+							i++;
+							continue;
+						}
+						else
+						{
+							char cplus[8];
+							strncpy(cplus, tmp, i);
+							strcpy(cplus + i, "\0");
+							plus = atoi(cplus);
+
+							char* msgtmp = StrStrI(texttmp, text + i);
+							return RollDice(ac, fromQQ, fromGroup, fromDiscuss, msgtmp, plus, secretly);
+						}
+					}
+				}
+				else
+				{
+					char* msgtmp = StrStrI(texttmp, text);
+					return RollDice(ac, fromQQ, fromGroup, fromDiscuss, msgtmp, plus, secretly);
+				}
+			}
+			else
+			{
+				char* msgtmp = StrStrI(texttmp, text);
+				return RollDice(ac, fromQQ, fromGroup, fromDiscuss, msgtmp, plus, secretly);
+			}
+		}
+	}
+
+	diceVect.clear();
+
+	SendMsg(ac, fromQQ, fromGroup, fromGroup, "您的输入格式有误,请参考帮助指令 .help");
+	return EVENT_BLOCK;
+}
+
+// 投掷骰子
+int Process::RollDice(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, char* msg, int plus, bool secretly)
+{
+	int sum = 0;
+	char formula[1024] = "";
+	char result[1024] = "";
+	char outputfmt[2048];
+	char output[2048];
+	sprintf_s(outputfmt, "* %%s 投掷 %%s : %%s = %%s");
 
 	const char* nick = GetNickName(ac, fromQQ, fromGroup, fromDiscuss);
 	if (NULL == nick)
 	{
+		diceVect.clear();
 		return EVENT_IGNORE;
 	}
 
-	if (msg)
+	//生成随机数种子
+	mtsrand((unsigned)(::timeGetTime() + fromQQ));
+
+	//获取队列长度
+	size_t len = diceVect.size();
+	if (len <= 0)
 	{
-		char tmp[100];
-		if (mysubstr(msg, "+", tmp))
-		{
-			plus = atoi(tmp);
-			sprintf(str, "* %s 投掷 %s : %dd%d+%d = ", nick, msg, throwtimes, sides, plus);
-		}
-		else if (mysubstr(msg, "-", tmp))
-		{
-			int minus = atoi(tmp);
-			sprintf(str, "* %s 投掷 %s : %dd%d-%d = ", nick, msg, throwtimes, sides, minus);
-			plus -= minus;
-		}
-		else
-		{
-			sprintf(str, "* %s 投掷 %s : %dd%d = ", nick, msg, throwtimes, sides);
-		}
+		//没有投掷骰子
+		return EVENT_IGNORE;
+	}
+
+	if (NULL == msg)
+	{
+		//标注为空
+		msg = "";
 	}
 	else
 	{
-		sprintf(str, "* %s 投掷 : %dd%d = ", nick, throwtimes, sides);
+		VS_StrLTrim(msg);//清除字符串前的空格
 	}
 
-	// 计算骰子总和
-	for (size_t i = 0; i < throwtimes;)
+	//组成算式
+	for (size_t i = 0; i < len; i++)
 	{
-		unsigned int randnum = mtirand() % sides + 1;
-		char string[10];
-		itoa(randnum, string, 10);
-		strcat(str, string);
-		sum += randnum;
-
-		i++;
-
-		if (throwtimes > i)
+		if (0 == diceVect[i].first)
 		{
-			strcat(str, "+");
+			char outputerr[256];
+			sprintf_s(outputerr, "* %s 丢了一团空气到了桌子上。", nick);
+			SendMsg(ac, fromQQ, fromGroup, fromGroup, outputerr);
+			diceVect.clear();
+			return EVENT_BLOCK;
 		}
-		else if(throwtimes == i && throwtimes != 1)
+		else if (0 == diceVect[i].second)
 		{
-			strcat(str, " = ");
-			itoa(sum, string, 10);
-			strcat(str, string);
+			char outputerr[256];
+			sprintf_s(outputerr, "* %s 拿出了一个异次元骰子 没有人看得到它的结果。", nick);
+			SendMsg(ac, fromQQ, fromGroup, fromGroup, outputerr);
+			diceVect.clear();
+			return EVENT_BLOCK;
 		}
 
-		Sleep(1);
+		char tmp[10];
+		if (i != 0 && diceVect[i].first > 0)
+		{
+			strcat_s(formula, 512, "+");
+		}
+
+		sprintf_s(tmp, "%dd%d", diceVect[i].first, diceVect[i].second);
+		strcat_s(formula, 512, tmp);
 	}
 
-	SendMsg(ac, fromQQ, fromGroup, fromDiscuss, str);
+	//组成结果
+	for (size_t i = 0; i < len; i++)
+	{
+		unsigned int dicenum = 0;
+		int rollret = 1;
+		char resulttmp[512] = "";
+		char tmp[10] = "";
+		char rolltmp[512] = "";
+		char fmt[512] = "";//fmt格式控制
 
+
+		if (diceVect[i].first > 0)//做加法
+		{
+			if (i != 0)
+			{
+				strcat_s(fmt, 512, "+");
+			}
+			dicenum = diceVect[i].first;
+		}
+		else if (diceVect[i].first < 0)//做减法
+		{
+			strcat_s(fmt, 512, "-");
+			dicenum -= diceVect[i].first;
+		}
+
+		//一次性投掷多个骰子的结果用括号标识
+		if (dicenum > 1)
+		{
+			strcat_s(fmt, 512, "(%s)");
+		}
+		else
+		{
+			strcat_s(fmt, 512, "%s");
+		}
+
+		//投掷骰子
+		for (int j = 0; j < dicenum; j++)
+		{
+			int sides = diceVect[i].second;
+			if (j != 0)
+			{
+				strcat_s(rolltmp, 512, "、");
+			}
+
+			//生成随机数
+			rollret = mtirand() % sides + 1;
+			if (diceVect[i].first > 0)
+			{
+				sum += rollret;
+			}
+			else
+			{
+				sum -= rollret;
+			}
+
+			itoa(rollret, tmp, 10);
+			strcat_s(rolltmp, 512, tmp);
+
+			Sleep(1);
+		}
+
+		//将此次投掷结果输入到结果字符串中
+		sprintf_s(resulttmp, fmt, rolltmp);
+		strcat_s(result, resulttmp);
+	}
+
+	if (0 != plus)
+	{
+		char tmp[10] = "";
+		itoa(plus, tmp, 10);
+
+		if (plus > 0)
+		{
+			strcat_s(formula, 512, "+");
+			strcat_s(result, 512, "+");
+		}
+
+		strcat_s(formula, 512, tmp);
+		strcat_s(result, 512, tmp);
+		sum += plus;
+	}
+
+	if (len > 1 || 0 != plus || diceVect[0].first > 1)
+	{
+		char tmp[10] = "";
+		itoa(sum, tmp, 10);
+		strcat_s(result, 512, " = ");
+		strcat_s(result, 512, tmp);
+	}
+
+	sprintf_s(output, outputfmt, nick, msg, formula, result);
+
+	if (secretly)
+	{
+		char outputh[512];
+		sprintf_s(outputh, "* %s 投了一把隐形骰子 你们这些笨蛋是看不见的", nick);
+		SendMsg(ac, fromQQ, fromGroup, fromGroup, outputh);
+		SendMsg(ac, fromQQ, 0, 0, output);
+	}
+	else
+	{
+		SendMsg(ac, fromQQ, fromGroup, fromDiscuss, output);
+	}
+
+	diceVect.clear();
 	return EVENT_BLOCK;
-	
 }
 
 // 随机COC6版属性
@@ -225,7 +745,7 @@ int Process::RollAttributes(int ac, int64_t fromQQ, int64_t fromGroup, int64_t f
 	}
 
 	char str[2048];
-	sprintf(str, "* %s 投掷COC 6版 属性 :", nick);
+	sprintf_s(str, "* %s 投掷COC 6版 属性 :", nick);
 
 	for (size_t i = 0; i < attributesnum; i++)
 	{
@@ -342,7 +862,7 @@ int Process::RollAttributes7(int ac, int64_t fromQQ, int64_t fromGroup, int64_t 
 		return EVENT_IGNORE;
 	}
 	char str[2048];
-	sprintf(str, "* %s 投掷COC 7版 属性 :", nick);
+	sprintf_s(str, "* %s 投掷COC 7版 属性 :", nick);
 
 	for (size_t i = 0; i < attributesnum; i++)
 	{
@@ -566,13 +1086,13 @@ int Process::SetNickName(int ac, int64_t fromQQ, int64_t fromGroup, int64_t from
 
 		MapNickName_[fromQQ] = newNick;
 
-		sprintf(str, " * %s 的新昵称是 %s", oldNick, newNick);
+		sprintf_s(str, " * %s 的新昵称是 %s", oldNick, newNick);
 
 		SendMsg(ac, fromQQ, fromGroup, fromDiscuss, str);
 	}
 	else
 	{
-		sprintf(str, " * %s 取消了自己的昵称", oldNick);
+		sprintf_s(str, " * %s 取消了自己的昵称", oldNick);
 
 		SendMsg(ac, fromQQ, fromGroup, fromDiscuss, str);
 	}
@@ -650,7 +1170,7 @@ int Process::RollFortune(int ac, int64_t fromQQ, int64_t fromGroup, int64_t from
 		SetFortune(fromQQ, fortune);
 	}
 	
-	sprintf(str, "* %s 今天的运势指数是 %u%% ! ", nick, fortune);
+	sprintf_s(str, "* %s 今天的运势指数是 %u%% ! ", nick, fortune);
 
 	for (int i = 0; i < fortune; i++)
 	{
@@ -734,4 +1254,22 @@ void Process::sbc_to_dbc(char *sbc, char *dbc)
 		}
 	}
 	*dbc = 0;
+}
+
+// 清除字符串前的空格
+void Process::VS_StrLTrim(char *pStr)
+{
+	char *pTmp = pStr;
+
+	while (*pTmp == ' ')
+	{
+		pTmp++;
+	}
+	while (*pTmp != '\0')
+	{
+		*pStr = *pTmp;
+		pStr++;
+		pTmp++;
+	}
+	*pStr = '\0';
 }
--- CQPdemo/ProcessMsg.h
+++ CQPdemo/ProcessMsg.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <time.h>
 #include <map>
+#include <vector>
+#include <utility>
 #include "QTool.h"
 #include "XAutoLock.h"
 
-typedef std::map<int64_t, char*>        MapNickName;
-typedef std::map<int64_t, unsigned int> MapFortune;
+typedef std::map<int64_t, char*>         MapNickName;//Map容器存放当前设置的昵称信息，前者为QQ号，后者为临时昵称
+typedef std::map<int64_t, unsigned int>  MapFortune;//Map容器存放本日运势，前者为QQ号，后者为运势指数
+typedef std::vector<std::pair<int, int>> DiceVector;//vector队列存放当前骰点的指令，前者为骰子数，后者为面数，当后者为负数时代表此次计算为减法
 
 class Process
 {
@@ -18,8 +21,11 @@ public:
 	// 处理消息
 	int ProcessMsg(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, const char* InputMsg);
 
+	// 普通骰点消息处理
+	int RollSegmentation(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, char* msg);
+
 	// 投掷骰子
-	int RollDice(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, unsigned int dicenum, unsigned int sides, char* msg);
+	int RollDice(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, char* msg, int plus, bool secretly);
 
 	// 随机COC6版属性
 	int RollAttributes(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, unsigned int attributesnum);
@@ -63,20 +69,26 @@ public:
 	// 全角转半角
 	void sbc_to_dbc(char *sbc, char *dbc);
 
+	// 清除字符串前的空格
+	void VS_StrLTrim(char *pStr);
+
 private:
 
 	XCritSec    csMapNickName_;
-	MapNickName MapNickName_;
+	MapNickName MapNickName_;//临时昵称容器
 
 	XCritSec    csMapFortune_;
-	MapFortune  MapFortune_;
+	MapFortune  MapFortune_;//运势指数容器，每日12点过后清空
+	int lastday;//运势容器对应的日期
 
 	XCritSec    csRollDice;
+	DiceVector	diceVect;//骰子队列
 
 	CQTool QTool;
 	CQ_TYPE_QQ QInfo;
 	CQ_Type_GroupMember QGroupInfo;
 
+	//指令集
 	const char* help;
 	const char* r;
 	const char* rd;
@@ -84,33 +96,4 @@ private:
 	const char* coc7;
 	const char* nn;
 	const char* jrrp;
-
-	int lastday;
 };
-
-// // 处理消息
-// int ProcessMsg(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, const char* InputMsg, char* OutputMsg);
-// 
-// // 投掷骰子
-// int RollDice(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, unsigned int dicenum, unsigned int sides, char* msg);
-// 
-// // 随机COC6版属性
-// int RollAttributes(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, unsigned int attributesnum);
-// 
-// // 随机COC7版属性
-// int RollAttributes7(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, unsigned int attributesnum);
-// 
-// // 投掷多个骰子
-// unsigned int MultiDiceSum(unsigned int dicenum, unsigned int sides);
-// 
-// // 判断str1是否以str2开头 如果是返回1 不是返回0 出错返回-1
-// int is_begin_with(const char * str1, char *str2);
-// 
-// // 从字符串str中查找str0，复制后面的内容到str1里面
-// int mysubstr(char *str, const char* str0, char str1[]);
-// 
-// // 获取昵称
-// const char* GetNickName(int ac, int64_t fromQQ);
-// 
-// //发送消息
-// void SendMsg(int ac, int64_t fromQQ, int64_t fromGroup, int64_t fromDiscuss, const char* msg);
